2315b1f13386c410e939195a78de84451da44171
Kitsada Nakhaphong <nakapong_k@silpakorn.edu>
2026-09-10 05:28:26 +0000

    moive

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,9 +1,22 @@
+import MovieGallery from "../Components/MovieGallery";
+import MovieCard from "../Components/Moviecard";
+import { useState } from "react";
+
 function Home() {
   return (
     <div className="p-8 text-center">
       <h1 className="text-3xl font-bold text-slate-800">🏠 หน้าแรก</h1>
       <p className="mt-2 text-slate-500">ยินดีต้อนรับสู่ MovieHub</p>
-    </div>
+
+      <div className="mx-auto mt-8 max-w-5xl">
+        <MovieCard title="inline mandatorian and Grogu" year={2026} />
+        <MovieCard title="Your Name." year={2016}/>
+        <MovieCard title="Spider-Man: Brand New Day" year={2026}/>
+        <h2 className="mt-8 text-2xl font-bold text-slate-800">ดูหนังเพิ่มเติม</h2>
+        <div classname="mx-auto mt-8 max-w-md"></div>
+          <MovieGallery />
+      </div>
+    </div >
   );
 }
 
